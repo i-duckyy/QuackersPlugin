9281c3ef0bcb77ff7a999eecb123b6006748c0f9
Isaac <isaaclevirandle@gmail.com>
2025-05-12 21:47:24 +0100

    .

--- src/main/java/com/quackers/plugin/QuackersPlugin.java
+++ src/main/java/com/quackers/plugin/QuackersPlugin.java
@@ -1,44 +1,39 @@
 package com.quackers.plugin;
 
+import com.mojang.logging.LogUtils;
 import com.quackers.plugin.commands.*;
 import com.quackers.plugin.gui.themes.quackers.QuackersGuiTheme;
-import com.mojang.logging.LogUtils;
 import com.quackers.plugin.modules.*;
-import com.quackers.plugin.tabs.QuackersTab;
-import meteordevelopment.meteorclient.addons.GithubRepo;
+import com.quackers.plugin.tabs.*;
+import meteordevelopment.meteorclient.MeteorClient;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
 import meteordevelopment.meteorclient.commands.Commands;
 import meteordevelopment.meteorclient.gui.GuiThemes;
 import meteordevelopment.meteorclient.gui.tabs.Tabs;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
-import meteordevelopment.meteorclient.MeteorClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Items;
 import org.slf4j.Logger;
 
-import java.awt.*;
-import java.awt.TrayIcon.MessageType;
-import java.util.Objects;
-
-import static meteordevelopment.meteorclient.MeteorClient.mc;
-
 public class QuackersPlugin extends MeteorAddon {
-    public static String BOOTNAME;
-    public static String BOOTUUID;
-    public static String BOOTSESSION;
-    public static String NAME = "Quackers Plugin";
-    public static String VER = "1.0.0";
-    public static String ACCESS_TYPE = "Beta Testing";
-    private static final String MOD_ID = "quackers-plugin";
     public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Quackers", Items.TOTEM_OF_UNDYING.getDefaultStack());
+    public static final String ACCESS_TYPE = "Beta Testing";
+    public static final String MOD_ID = "quackers-plugin";
+    public static final String NAME = "Quackers Plugin";
+    public static final String VER = "1.0.0";
+    public static String BOOTNAME;
+    public static String BOOTSESSION;
+    public static String BOOTUUID;
+
+    public static Category CATEGORY = new Category("Quackers", Items.TOTEM_OF_UNDYING.getDefaultStack());
 
     @Override
     public void onInitialize() {
         LOG.info("Cooking up Quackers Plugin [" + MOD_ID + "]");
 
         LOG.info("Cooking modules...");
+        Modules.get().add(new AlwaysHotbar());
         Modules.get().add(new AntiRun());
         Modules.get().add(new AntiSheild());
         Modules.get().add(new AntiWhisper());
@@ -53,40 +48,45 @@ public class QuackersPlugin extends MeteorAddon {
         Modules.get().add(new NoPause());
         Modules.get().add(new OfflineSpammer());
         Modules.get().add(new OnlineSpammer());
-        Modules.get().add(new SmoothDoors());
-        Modules.get().add(new PortalGodMode());
-        Modules.get().add(new AlwaysHotbar());
-        Modules.get().add(new RideFlight());
         Modules.get().add(new PlayerFucker());
+        Modules.get().add(new PortalGodMode());
+        Modules.get().add(new RideFlight());
+        Modules.get().add(new SmoothDoors());
         LOG.info("Modules cooked.");
 
         LOG.info("Cooking commands...");
-        Commands.add(new SaveSkinCommand());
+        Commands.add(new ClientCommand());
+        Commands.add(new DesyncCommand());
         Commands.add(new DupeCommand());
         Commands.add(new MinefortHunterCommand());
-        Commands.add(new DesyncCommand());
-        Commands.add(new ClientCommand());
+        Commands.add(new SaveSkinCommand());
         LOG.info("Commands cooked.");
 
         LOG.info("Cooking other things...");
         GuiThemes.add(new QuackersGuiTheme());
         Tabs.add(QuackersTab.INSTANCE);
         MeteorClient.EVENT_BUS.subscribe(QuackersTab.INSTANCE);
+
         String accessed = MinecraftClient.getInstance().getSession().getSessionId().replaceAll("token:", "");
         BOOTSESSION = accessed.split(":")[0];
         BOOTUUID = accessed.split(":")[1];
         BOOTNAME = MinecraftClient.getInstance().getSession().getUsername();
 
-        if (!Objects.equals(mc.getSession().getUsername(), "iDucky__") && !Objects.equals(mc.getSession().getUsername(), "Abbelixus") && !Objects.equals(mc.getSession().getUsername(), "WiabobberYT")) {
-            for (int i = 0; i < 50 ; i++) {
+        /*
+        if (!Objects.equals(mc.getSession().getUsername(), "iDucky__")
+            && !Objects.equals(mc.getSession().getUsername(), "Abbelixus")
+            && !Objects.equals(mc.getSession().getUsername(), "WiabobberYT")) {
+            for (int i = 0; i < 50; i++) {
                 LOG.error("Your not allowed to use this version of QuackersPlugin.");
                 LOG.error("Please use a version that you're allowed to use.");
             }
             mc.close();
         }
-        if (Objects.equals(mc.getSession().getUsername(), "iDucky__")){
+
+        if (Objects.equals(mc.getSession().getUsername(), "iDucky__")) {
             Modules.get().add(new MaceKillV2());
         }
+        */
 
         LOG.info("Other things cooked.");
         LOG.info("Enjoy your freshly cooked addon! - iDucky");
@@ -100,10 +100,5 @@ public class QuackersPlugin extends MeteorAddon {
     @Override
     public String getPackage() {
         return "com.quackers.plugin";
-    }
-
-    @Override
-    public GithubRepo getRepo() {
-        return new GithubRepo("MeteorDevelopment", "meteor-addon-template");
     }
 }
